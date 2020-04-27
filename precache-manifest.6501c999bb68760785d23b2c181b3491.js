062cdeac4d9f00418dbf23e040efb04adda52807
Walafi02 <walafif@yahoo.com>
2020-04-27 18:12:38 +0000

    Updates

---
+++ precache-manifest.6501c999bb68760785d23b2c181b3491.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c664bb17b813caf37405111f873dcb6c",
+    "url": "/make-your-pizza/index.html"
+  },
+  {
+    "revision": "b7f6b0135b3a865f4038",
+    "url": "/make-your-pizza/static/js/2.0868957b.chunk.js"
+  },
+  {
+    "revision": "ca5ca5a37f61c802e3bf9aacfcfefac9",
+    "url": "/make-your-pizza/static/js/2.0868957b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d64ec3117d1d1113a244",
+    "url": "/make-your-pizza/static/js/main.42e1b8e0.chunk.js"
+  },
+  {
+    "revision": "654c72ab582f1507e31a",
+    "url": "/make-your-pizza/static/js/runtime-main.d451b003.js"
+  },
+  {
+    "revision": "71a7b90e194ddb55581a10952edc13ab",
+    "url": "/make-your-pizza/static/media/pizza-background.71a7b90e.jpeg"
+  },
+  {
+    "revision": "126c585eb36ad518cebcef215b9b2e7f",
+    "url": "/make-your-pizza/static/media/pizza.126c585e.png"
+  }
+]);
